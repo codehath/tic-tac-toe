36e079e4b4f3ae81ac04bebdb711a85e7454e2c0
Farhath <codingfarhath@gmail.com>
2023-12-21 04:17:56 +0000

    feat: add replay option and input validation

--- main.py
+++ main.py
@@ -14,11 +14,21 @@ Step 3: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K.
 Step 4: To try out cursor on your own projects, go to the file menu (top left) and open a folder.
 """
 
+'''
+Make first number col, second row
+Add option to replay game
+Stop moves from being overwritten
+
+Add Validation
+'''
 
 # Generate me a CLI-based game of TicTacToe
 def print_board(board):
-    for row in board:
-        print(" ".join(row))
+    for index, row in enumerate(board):
+        print(" | ".join(row))
+        # if index != len(board)-1:
+        #     print("-"*9)
+    print("-"*33)
 
 
 def check_win(board):
@@ -37,20 +47,45 @@ def check_win(board):
         return True
     return False
 
+def error_message():
+    print("!!! Invalid move, try again.")
+    print("-"*33)
 
 def tictactoe():
+    print("New Game =====\n" + "-"*33)
     board = [[" " for _ in range(3)] for _ in range(3)]
     player = "X"
+    invalid = False
     while True:
-        print_board(board)
-        print("Player", player, "turn")
-        move = input("Enter your move (row col): ")
-        row, col = map(int, move.split())
+        if not invalid:
+            print_board(board)
+        print(f"- Player {player} turn")
+        move = input("Enter your move (col row): ")
+        input_move = [int(char)-1 for char in move if char.isdigit() and 0 <= int(char)-1 <= 2]
+
+        if len(move) != 2 or len(input_move) != 2:
+            error_message()
+            invalid = True
+            continue
+
+        col, row = input_move[0], input_move[1]
+        if board[row][col]  != " ":
+            error_message()
+            invalid = True
+            continue
+
         board[row][col] = player
+        invalid = False
         if check_win(board):
-            print("Player", player, "wins!")
+            print("="*33)
+            print(f"::: Player {player} wins! :::")
             break
         player = "O" if player == "X" else "X"
+    
 
 
-tictactoe()
+if __name__ == "__main__":
+    tictactoe()
+    replay = input("Do you want to play again? (y/n): ")
+    if replay.lower() == "y":
+        tictactoe()
